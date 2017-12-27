27b1243b5e3a67b81f57ed54f082b3861db52e6c
german <germanjongewaard@gmail.com>
2017-12-27 18:52:23 +0100

    toolbar commit

--- app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/activities/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/activities/MainActivity.java
@@ -1,10 +1,12 @@
-package dev.com.jongewaard.tabs_y_toolbar;
+package dev.com.jongewaard.tabs_y_toolbar.activities;
 
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+
+import dev.com.jongewaard.tabs_y_toolbar.R;
 
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/fragments/FirstFragment.java
@@ -0,0 +1,8 @@
+package dev.com.jongewaard.tabs_y_toolbar.fragments;
+
+/**
+ * Created by german on 27-12-17.
+ */
+
+public class FirstFragment {
+}
