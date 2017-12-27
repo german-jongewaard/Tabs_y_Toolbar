59d24f5584c2c16749bdfb8be4a06b0ee3ada7ed
german <germanjongewaard@gmail.com>
2017-12-27 18:53:39 +0100

    toolbar commit

--- app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/fragments/FirstFragment.java
+++ app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/fragments/FirstFragment.java
@@ -1,8 +1,30 @@
 package dev.com.jongewaard.tabs_y_toolbar.fragments;
 
-/**
- * Created by german on 27-12-17.
- */
 
-public class FirstFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dev.com.jongewaard.tabs_y_toolbar.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
 }
