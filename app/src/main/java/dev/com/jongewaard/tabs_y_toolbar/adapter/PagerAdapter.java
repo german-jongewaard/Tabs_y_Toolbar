26e15b75be1c70ff9e3863de58c7c209d8cfaf3b
german <germanjongewaard@gmail.com>
2017-12-27 19:41:51 +0100

    toolbar commit . add Fragments

--- app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/adapter/PagerAdapter.java
+++ app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/adapter/PagerAdapter.java
@@ -4,6 +4,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import dev.com.jongewaard.tabs_y_toolbar.fragments.FirstFragment;
+import dev.com.jongewaard.tabs_y_toolbar.fragments.SecondFragment;
+import dev.com.jongewaard.tabs_y_toolbar.fragments.ThirdFragment;
+
 /**
  * Created by german on 27-12-17.
  */
@@ -23,9 +27,19 @@ public class PagerAdapter extends FragmentStatePagerAdapter{
     @Override
     public Fragment getItem(int position) {
 
+        switch (position){
 
+            case 0:
+                return new FirstFragment();
 
-        return null;
+            case 1:
+                return new SecondFragment();
+
+            case 2:
+                return new ThirdFragment();
+            default:
+                return null;
+        }
     }
 
     @Override
