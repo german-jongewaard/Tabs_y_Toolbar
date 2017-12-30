b6d3491baaab83fd3905607a5b6bbfe6bcf173ee
german <germanjongewaard@gmail.com>
2017-12-30 01:56:21 +0100

    toolbar commit . add Fragments

--- app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/activities/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Toast;
 
 import dev.com.jongewaard.tabs_y_toolbar.R;
 import dev.com.jongewaard.tabs_y_toolbar.adapter.PagerAdapter;
@@ -42,17 +43,20 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override //cuando selecciono el Tab
             public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Selected " + tab.getText(), Toast.LENGTH_SHORT).show();
                 int position = tab.getPosition();
                 viewPager.setCurrentItem(position);
             }
 
             @Override //cuando estaba activo y deja de estarlo
             public void onTabUnselected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Unselected " + tab.getText(), Toast.LENGTH_SHORT).show();
 
             }
 
             @Override //cuando selecciono el mismo Tab que estaba activo
             public void onTabReselected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Reselected " + tab.getText(), Toast.LENGTH_SHORT).show();
 
             }
         });
