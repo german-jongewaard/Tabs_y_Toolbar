77dde4b693a2612b88b3fbc33bbdc746976fed5b
german <germanjongewaard@gmail.com>
2017-12-30 02:44:25 +0100

    toolbar commit . add Fragments

---
+++ app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/adapters/PersonAdapter.java
@@ -0,0 +1,94 @@
+package dev.com.jongewaard.tabs_y_toolbar.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by german on 27-12-17.
+ */
+
+public class PersonAdapter extends BaseAdapter {
+
+
+    private Context context;
+    private int layout;
+    private List<Person> persons;
+
+
+    public PersonAdapter(Context context, int layout, List<Person> persons) {
+        this.context = context;
+        this.layout = layout;
+        this.persons = persons;
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return persons.size();
+    }
+
+    @Override
+    public Person getItem(int position) {
+        return persons.get(position);
+    }
+
+    @Override
+    public long getItemId(int id) {
+        return id;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+
+
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(layout, null);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.textViewPersonName);
+            holder.country = (TextView) convertView.findViewById(R.id.textViewPersonCountry);
+            holder.image = (ImageView) convertView.findViewById(R.id.imageViewFlag);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Person currentPerson = getItem(position);
+        // Person currentPerson = persons.get(position);
+
+        // holder.name.setText(currentPerson.getName());
+        // holder.country.setText(currentPerson.getCountry().getName());
+
+        holder.name.setText(currentPerson.getName());
+        holder.country.setText(currentPerson.getCountry().getName());
+
+
+        String url = currentPerson.getCountry().getFlagURL();
+        Picasso.with(context).load(url).into(holder.image);
+
+
+
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        private TextView name;
+        private TextView country;
+        private ImageView image;
+    }
+
+
+}
--- app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/models/Person.java
+++ app/src/main/java/dev/com/jongewaard/tabs_y_toolbar/models/Person.java
@@ -5,4 +5,29 @@ package dev.com.jongewaard.tabs_y_toolbar.models;
  */
 
 public class Person {
+
+    private String name;
+    private Country country;
+
+    public Person(String name, Country country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
 }
+
